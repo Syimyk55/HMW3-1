38c338dcb790095fea0282a4cf172c787104ad3e
Syimyk55 <114605451+Syimyk55@users.noreply.github.com>
2022-12-10 15:27:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hmw3_1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hmw3_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.rp_text);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+
+            }
+        });
+    }
+
+}
